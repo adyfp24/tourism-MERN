0ed699579fd9e3f5d158ddfebf46abdb74332a04
adyfp24 <adyfp24@gmail.com>
2024-05-25 22:52:35 +0700

    feat: base user service & api helper

--- client/src/services/userService.jsx
+++ client/src/services/userService.jsx
@@ -1,20 +1,22 @@
 import axios from 'axios';
+import apiHelper from '../utils/apiHelper';
 
-const userService = () => {
+export const registService = async (userData) => {
+  try {
+    const response = await axios.post(`${apiHelper.baseUrl}/register`, userData);
+    return response.data.data;
+  } catch (error) {
+    throw error.response ? error.response.data : new Error('Registration failed');
+  }
+};
 
-  const registService = async () => {
+export const loginService = async (userData) => {
+  const response = await axios.post(`${apiHelper.baseUrl}/login`, userData);
+  return response.data.data;
+};
 
-  };
+export const logoutService = async () => {
+  const response = await axios.post(`${apiHelper.baseUrl}/login`);
+  return response.data
+};
 
-  const loginService = async () => {
-
-  };
-
-  const logoutService = async () => {
-    
-  }; 
-
-  return { registService, loginService, logoutService};
-}
-
-export default userService
--- client/src/utils/apiHelper.jsx
+++ client/src/utils/apiHelper.jsx
@@ -1,6 +1,5 @@
-
 const apiHelper = {
-  
+  baseUrl: 'http://localhost:3000/api/v1'
 }
 
 export default apiHelper
